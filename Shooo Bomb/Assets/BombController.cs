8547dffa6122b611d00622aa5e33a743102f101d
MADMONKEY\석규 <tlqkfkak88@naver.com>
2018-09-27 01:02:43 +0900

    컨트롤러 스크립트 iTween사용하여 전진 디폴트

---
+++ Shooo Bomb/Assets/BombController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour {
+
+ 
+	void Start () {
+	}
+	
+	void Update () {
+        //디폴트로 전진하기
+        iTween.MoveBy(this.gameObject, iTween.Hash("z", 1.1f,                              //z축 1.1 위치 까지
+                                                        "easeType", iTween.EaseType.linear,     //일정한 속도로
+                                                        "speed", 1.0f                           //1의 스피드로 이동
+                                                        ));
+		
+	}
+}
