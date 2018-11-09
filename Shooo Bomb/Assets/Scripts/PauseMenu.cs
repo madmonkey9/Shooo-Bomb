154caa88451b9fac3eb239cf13f40b888cadfc4f
JunKwon <junkwon96@kookmin.ac.kr>
2018-11-09 15:45:19 +0900

    합병 후 커밋

---
+++ Shooo Bomb/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject PauseUI;
+
+    private bool paused = false;
+
+    void Start()
+    {
+        PauseUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            paused = !paused;
+        }
+        if (paused)
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0.001f;
+        }
+
+        if (!paused)
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Resume()
+    {
+        paused = !paused;
+    }
+
+    public void Restaret()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("IntroScene");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
