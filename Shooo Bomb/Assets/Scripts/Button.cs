104a879ca24d45386fafd23e391f3c690f6af06e
KMU\yellowoov <yellowoov@kookmin.ac.kr>
2018-12-03 00:12:54 +0900

    continue버튼 클릭시 스타팅포인트에서 시작

--- Shooo Bomb/Assets/Scripts/Button.cs
+++ Shooo Bomb/Assets/Scripts/Button.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Button : MonoBehaviour {
 
+    GameObject stage2point = null;
+    GameObject stage3point = null;
+    GameObject stage4point = null;
+
     // Start_Game() 함수는 IntroScene이 있는 Scenes 폴더 내의 GameScene을 불러온다.
     public void Start_Game()
     {
@@ -22,5 +26,32 @@ public class Button : MonoBehaviour {
     {
         Time.timeScale = 1.0f;
         LoadingSceneManager.LoadScene("IntroScene");
+    }
+
+    public void stage1clear()
+    {
+        stage2point = GameObject.Find("Level2StartPoint"); // Stage2 스타팅포인트를 찾아서 저장
+        Time.timeScale = 1.0f;
+
+        GameObject.Find("UICanvas").transform.Find("Stage1ClearPanel").gameObject.SetActive(false); // 클리어 UI 비활성화
+        PlayerHealth.instance.transform.position = stage2point.transform.position; // 플레이어의 좌표를 스타팅포인트로 이동
+    }
+
+    public void stage2clear()
+    {
+        stage3point = GameObject.Find("Level3StartPoint"); // Stage3 스타팅포인트를 찾아서 저장
+        Time.timeScale = 1.0f;
+
+        GameObject.Find("UICanvas").transform.Find("Stage2ClearPanel").gameObject.SetActive(false); // 클리어 UI 비활성화
+        PlayerHealth.instance.transform.position = stage3point.transform.position; // 플레이어의 좌표를 스타팅포인트로 이동
+    }
+
+    public void stage3clear()
+    {
+        stage4point = GameObject.Find("Level4StartPoint"); // Stage4 스타팅포인트를 찾아서 저장
+        Time.timeScale = 1.0f;
+
+        GameObject.Find("UICanvas").transform.Find("Stage3ClearPanel").gameObject.SetActive(false); // 클리어 UI 비활성화
+        PlayerHealth.instance.transform.position = stage4point.transform.position; // 플레이어의 좌표를 스타팅포인트로 이동
     }
 }
